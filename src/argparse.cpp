ccbbfb98736b80885b7f29b47880ee5bae0f1ed4
barts <bartsteensma@outlook.com>
2023-07-18 12:21:46 +0200

    docs: added docstrings

--- src/argparse.cpp
+++ src/argparse.cpp
@@ -2,6 +2,12 @@
 #include <cxxopts.hpp>
 #include <iostream>
 
+/**
+ * @brief Construct a new Arg Parse:: Arg Parse object
+ *
+ * @param argc The number of arguments
+ * @param argv The arguments
+ */
 ArgParse::ArgParse(int argc, char *argv[])
     : argc(argc),
       argv(argv),
@@ -9,6 +15,7 @@ ArgParse::ArgParse(int argc, char *argv[])
       description(
           "Command line interface for taking snapshots with a camera.\n"),
       options(program, description) {
+
   // clang-format off
   options.add_options()
     ("h,help", "Print help")
@@ -16,8 +23,18 @@ ArgParse::ArgParse(int argc, char *argv[])
   // clang-format on
 }
 
+/**
+ * @brief Parse the arguments
+ *
+ * @return a cxxopts::ParseResult object
+ */
 cxxopts::ParseResult ArgParse::parse() { return options.parse(argc, argv); }
 
+/**
+ * @brief Get the help string
+ *
+ * @return The help string
+ */
 std::string ArgParse::help() { return options.help(); }
 
 ArgParse::~ArgParse() {}
--- tests/argparse_test.cpp
+++ tests/argparse_test.cpp
@@ -5,9 +5,12 @@
 
 #include <argparse.h>
 
-TEST(parseTestHelp, BasicAssertions) {
+/**
+ * @brief Tests the ArgParse::parse() function.
+ */
+TEST(parseTestParse, BasicAssertions) {
   int argc = 2;
-  char *argv[2] = {const_cast<char *>("./argparse_test"),
+  char *argv[2] = {const_cast<char *>("./test"),
                    const_cast<char *>("--help")};
 
   ArgParse parser(argc, argv);
