9bd1fc6d7adc2b27c8b1fc2e94338bc0022f2133
barts <b.steensma@fleetcleaner.com>
2023-07-24 09:22:15 +0200

    bugfix: moved the private functions to the source file.

--- src/list.cpp
+++ src/list.cpp
@@ -8,18 +8,52 @@
 using Table = std::vector<std::vector<std::string>>;
 
 /**
- * @brief VideoScene::getCameraDevice
+ * @brief getColumnWidths
  *
- * Uses the QMediaDevices::videoInputs() function to get a list of cameras.
- * This QList is then cast to a std::string. Next the std::string is formatted
- * such that it can easily be interpreted by the user.
+ * Determines the width of each column in a Table. The width of each column is
+ * determined by the longest string in that column.
  *
- * @return The available cameras as a std::string.
- */
-std::string listCameras() {
-  const QList<QCameraDevice> cameras = QMediaDevices::videoInputs();
-  Table table = qlistToTable(cameras);
-  return tableToString(table);
+ * @param table The table to determine the column widths of.
+ * @return A vector containing the width of each column.
+ **/
+std::vector<int> getColumnWidths(Table table) {
+  std::vector<int> columnWidths;
+  for (int i = 0; i < table.at(0).size(); i++) {
+    int columnWidth = 0;
+    for (int j = 0; j < table.size(); j++) {
+      std::string cell = table.at(j).at(i);
+      if (cell.length() > columnWidth) {
+        columnWidth = cell.length();
+      }
+    }
+    columnWidths.push_back(columnWidth);
+  }
+  return columnWidths;
+}
+
+/**
+ * @brief tableToString
+ *
+ * Converts a Table to a std::string. The width of each column is determined by
+ * the longest string in that column.
+ *
+ * @param table The table to convert.
+ * @return The table as a std::string.
+ **/
+std::string tableToString(Table table, std::string separator = " | ") {
+  std::string result = "";
+  std::vector<int> columnWidths = getColumnWidths(table);
+  for (int i = 0; i < table.size(); i++) {
+    std::vector<std::string> row = table.at(i);
+    for (int j = 0; j < row.size(); j++) {
+      std::string cell = row.at(j);
+      result += cell;
+      result += std::string(columnWidths.at(j) - cell.length(), ' ');
+      result += separator;
+    }
+    result += "\n";
+  }
+  return result;
 }
 
 /**
@@ -49,50 +83,16 @@ Table qlistToTable(QList<QCameraDevice> cameras) {
 }
 
 /**
- * @brief tableToString
+ * @brief VideoScene::getCameraDevice
  *
- * Converts a Table to a std::string. The width of each column is determined by
- * the longest string in that column.
+ * Uses the QMediaDevices::videoInputs() function to get a list of cameras.
+ * This QList is then cast to a std::string. Next the std::string is formatted
+ * such that it can easily be interpreted by the user.
  *
- * @param table The table to convert.
- * @return The table as a std::string.
- **/
-std::string tableToString(Table table, std::string separator) {
-  std::string result = "";
-  std::vector<int> columnWidths = getColumnWidths(table);
-  for (int i = 0; i < table.size(); i++) {
-    std::vector<std::string> row = table.at(i);
-    for (int j = 0; j < row.size(); j++) {
-      std::string cell = row.at(j);
-      result += cell;
-      result += std::string(columnWidths.at(j) - cell.length(), ' ');
-      result += separator;
-    }
-    result += "\n";
-  }
-  return result;
-}
-
-/**
- * @brief getColumnWidths
- *
- * Determines the width of each column in a Table. The width of each column is
- * determined by the longest string in that column.
- *
- * @param table The table to determine the column widths of.
- * @return A vector containing the width of each column.
- **/
-std::vector<int> getColumnWidths(Table table) {
-  std::vector<int> columnWidths;
-  for (int i = 0; i < table.at(0).size(); i++) {
-    int columnWidth = 0;
-    for (int j = 0; j < table.size(); j++) {
-      std::string cell = table.at(j).at(i);
-      if (cell.length() > columnWidth) {
-        columnWidth = cell.length();
-      }
-    }
-    columnWidths.push_back(columnWidth);
-  }
-  return columnWidths;
+ * @return The available cameras as a std::string.
+ */
+std::string listCameras() {
+  const QList<QCameraDevice> cameras = QMediaDevices::videoInputs();
+  Table table = qlistToTable(cameras);
+  return tableToString(table);
 }
--- src/list.hpp
+++ src/list.hpp
@@ -1,11 +1,3 @@
-#include <QCameraDevice>
-#include <QMediaDevices>
 #include <string>
-#include <vector>
-
-using Table = std::vector<std::vector<std::string>>;
 
 std::string listCameras();
-Table qlistToTable(QList<QCameraDevice> cameras);
-std::string tableToString(Table table, std::string separator = " | ");
-/* std::vector<int> getColumnWidths(Table table); */
