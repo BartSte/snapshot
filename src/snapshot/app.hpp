9d77cda6dc9a403f0821c5da35ef615170ffd380
barts <bartsteensma@outlook.com>
2023-11-08 21:28:30 +0100

    refactor: remove app.hpp and gui.cpp

--- src/snapshot/app.hpp
+++ src/snapshot/app.hpp
@@ -7,8 +7,8 @@
 #include <memory>
 #include <qapplication.h>
 #include <video/record.hpp>
+#include <gui/mainwindow.hpp>
 
-#include "snapshot/gui.hpp"
 #include "video/connect.hpp"
 
 class App : public QApplication {
@@ -25,15 +25,14 @@ class App : public QApplication {
   static const path PATH_CONFIG;
   static const path DEBUG_VIDEO;
 
-  std::unique_ptr<Gui> gui;
   std::unique_ptr<Recorder> recorder;
   std::unique_ptr<BaseVideo> video;
+  std::unique_ptr<MainWindow> window;
   ArgParse parser;
   ptree settings;
 
   ptree parseConfig(const cxxopts::ParseResult &args);
-  void initLogger(const ptree &config);
-  void printHelp();
+  bool printHelp();
   void enableDebugMode();
   void list();
   void connectCamera();
