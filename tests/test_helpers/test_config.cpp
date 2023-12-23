be57ccbaccc7bd6b80708682a4f6cbab2c6cef62
barts <bartsteensma@outlook.com>
2023-12-23 16:32:19 +0100

    improvement: added tests for config checks

--- tests/test_helpers/test_config.cpp
+++ tests/test_helpers/test_config.cpp
@@ -1,5 +1,6 @@
 #include <boost/dll.hpp>
 #include <boost/property_tree/ptree_fwd.hpp>
+#include <cstdint>
 #include <filesystem>
 #include <gtest/gtest.h>
 #include <helpers/config.hpp>
@@ -73,4 +74,93 @@ TEST(testConfig, parseConfigsTestIncomplete) {
   ASSERT_EQ(config.get<std::string>("camera"), "default");
   ASSERT_EQ(config.get<std::string>("loglevel"), "info");
   ASSERT_EQ(config.get<std::string>("pattern"), "foo");
+}
+
+/*
+ * @brief tryCatchSucceed
+ *
+ * Takes a function as argument and executes it in a try catch block. If an
+ * std::invalid_argument is thrown, the test succeeds, otherwise it fails.
+ * @param function The function to execute.
+ */
+void tryCatchSucceed(std::function<void()> function) {
+  try {
+    function();
+    FAIL();
+  } catch (std::invalid_argument &e) {
+    SUCCEED();
+  }
+}
+
+/**
+ * @brief tryCatchFail
+ *
+ * Takes a function as argument and executes it in a try catch block. If an
+ * std::invalid_argument is thrown, the test fails, otherwise it succeeds.
+ *
+ * @param function The function to execute.
+ */
+void tryCatchFail(std::function<void()> function) {
+  try {
+    function();
+    SUCCEED();
+  } catch (std::invalid_argument &e) {
+    FAIL();
+  }
+}
+
+TEST(testConfig, testCheckLogLevel) {
+  ptree config;
+  auto func = [&config]() { config::checkLogLevel(config); };
+
+  config.put("loglevel", "foo");
+  tryCatchSucceed(func);
+
+  config.put("loglevel", "warning");
+  tryCatchFail(func);
+}
+
+TEST(testConfig, testCheckChrono) {
+  ptree config;
+  auto func = [&config]() { config::checkChrono(config); };
+
+  config.put("timeout", "foo");
+  config.put("interval", "foo");
+  config.put("duration", "foo");
+  tryCatchSucceed(func);
+
+  config.put("timeout", "1");
+  config.put("interval", "1");
+  config.put("duration", "1");
+  tryCatchFail(func);
+}
+
+TEST(testConfig, testCheckScientificNotation) {
+  ptree config;
+  auto func = [&config]() { config::checkScientificNotation(config); };
+
+  config.put("max-bytes", "foo");
+  tryCatchSucceed(func);
+  config.put("max-bytes", "-1e10");
+  tryCatchSucceed(func);
+
+  config.put("max-bytes", "1e10");
+  tryCatchFail(func);
+}
+
+TEST(testConfig, testScientificNotation) {
+  tryCatchSucceed([]() { config::scientificToUint64("foo"); });
+
+  try {
+    uint64_t value = config::scientificToUint64("1e10");
+    ASSERT_EQ(value, 10000000000);
+
+    value = config::scientificToUint64("1e-10");
+    ASSERT_EQ(value, 0);
+
+    value = config::scientificToUint64("-1e0");
+    ASSERT_EQ(value, UINT64_MAX);
+  } catch (std::invalid_argument &e) {
+    FAIL();
+  }
 }
